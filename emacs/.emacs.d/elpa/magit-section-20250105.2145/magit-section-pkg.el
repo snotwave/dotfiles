4f195761fa4b3736049595ff510a993de04116b3
isabel <ixthegoat@gmail.com>
2025-01-06 10:32:28 -0500

    battery

---
+++ emacs/.emacs.d/elpa/magit-section-20250105.2145/magit-section-pkg.el
@@ -0,0 +1,13 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "magit-section" "20250105.2145"
+  "Sections for read-only buffers."
+  '((emacs  "27.1")
+    (compat "30.0.1.0")
+    (dash   "2.19.1")
+    (seq    "2.24"))
+  :url "https://github.com/magit/magit"
+  :commit "7f47299581abb6f77870cb5abdae159fcd35b3d5"
+  :revdesc "7f47299581ab"
+  :keywords '("tools")
+  :authors '(("Jonas Bernoulli" . "emacs.magit@jonas.bernoulli.dev"))
+  :maintainers '(("Jonas Bernoulli" . "emacs.magit@jonas.bernoulli.dev")))
