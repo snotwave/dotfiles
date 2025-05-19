27059d4a4812f3a88270e2567d909b4f183d0add
isabel <ixthegoat@gmail.com>
2025-05-19 14:26:37 -0400

    removed old nvim

--- nvim-old/.config/nvim/init.lua
+++
@@ -1,6 +0,0 @@
-require("core.filetypes")
-require("core.plugins")
-require("core.plugin_config")
-require("core.misc_keybinds")
-require("core.basic")
-require("core.startup")
--- nvim-old/.config/nvim/lua/core/plugin_config/CHADtree.lua
+++
@@ -1,7 +0,0 @@
-vim.api.nvim_set_keymap('n', '<F6>', ":CHADopen<CR>", { noremap = true, silent = true })
-
-local chadtree_settings = {
-    ["view.open_direction"] = "right"
-}
-
-vim.api.nvim_set_var("chadtree_settings", chadtree_settings)
